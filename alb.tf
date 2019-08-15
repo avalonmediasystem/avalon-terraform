87a918e334af23b1bbc2d2426095124710ee4a2e
Phuong Dinh <phuongdh@gmail.com>
2019-08-15 15:32:18 -0400

    Forward http traffic to https

--- alb.tf
+++ alb.tf
@@ -56,6 +56,22 @@ resource "aws_route53_record" "alb_streaming" {
   records = ["${aws_alb.alb.dns_name}"]
 }
 
+resource "aws_lb_listener" "alb_forward_https" {
+  load_balancer_arn = "${aws_alb.alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_alb_listener" "alb_listener" {  
   load_balancer_arn = "${aws_alb.alb.arn}"  
   port              = "443"
